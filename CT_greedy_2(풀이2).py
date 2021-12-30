404e69a4169767da125f13ce28b90a6d737482bf
JaeSeong Kwon <zxc715169@gmail.com>
2021-12-30 13:56:13 +0900

    CT greedy 세 가지 풀이법(설명 수수정)

--- CT_greedy_2(풀이2).py
+++ CT_greedy_2(풀이2).py
@@ -6,7 +6,7 @@ result = 0
 
 for _ in range(N):
     card = list(map(int,sys.stdin.readline().split()))
-    min_card = min(card)
-    result = max(result, min_card) # 계속 result를 큰 값으로 갱신하는 방식이다.
+    min_card = min(card) # card 리스트 중 가장 작은 카드
+    result = max(result, min_card) # 가장 작은 수의 카드들 중 가장 큰 수의 카드를 계속 갱신하는 방식이다.
 
 print(result)
